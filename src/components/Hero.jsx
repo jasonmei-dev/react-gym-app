95e91ba99a658ccac91a44251b6e7b1f835b771b
jmei403 <jason.mei403@gmail.com>
2024-09-05 17:07:12 -0400

    SectionWrapper component, workouts data, and minor syling changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
       <div className="flex flex-col gap-4">
         <p>It's time to get</p>
         <h1 className="uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
-          Swole<span>normous</span>
+          Swole<span className="text-blue-400">normous</span>
         </h1>
       </div>
       <p className="text-sm md:text-base font-light">
