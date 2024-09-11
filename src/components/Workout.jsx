3c6bb3513da6a5bdc95451adfbdfa94820bc4aef
jmei403 <jason.mei403@gmail.com>
2024-09-11 13:05:42 -0400

    render workout with exercise cards

--- src/components/Workout.jsx
+++ src/components/Workout.jsx
@@ -1,5 +1,16 @@
-const Workout = () => {
-  return <div>Workout</div>;
+import SectionWrapper from './SectionWrapper';
+import ExerciseCard from './ExerciseCard';
+
+const Workout = ({ workout }) => {
+  return (
+    <SectionWrapper header={'Welcome to'} title={['The', 'DANGER', 'zone']}>
+      <div className="flex flex-col gap-4">
+        {workout.map((exercise, i) => {
+          return <ExerciseCard exercise={exercise} index={i} key={i} />;
+        })}
+      </div>
+    </SectionWrapper>
+  );
 };
 
 export default Workout;
